68dca7681a06ee4500783830dbaa87b4e3184b44
Neil Gordon <neilng2109@gmail.com>
2026-01-03 14:02:49 +0100

    Div or main issue accessrequestpage

--- src/pages/AccessRequestsPage.jsx
+++ src/pages/AccessRequestsPage.jsx
@@ -206,8 +206,7 @@ export default function AccessRequestsPage() {
         )}
       </main>
 	  
-	  </main>
-
+	 
       {/* Password Display Modal */}
       {approvedPassword && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4 z-50">
